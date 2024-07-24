a0043ee72a7cce322ab9a807bc7bf6a5f0c84d48
Mohammed Zaid <mohammed.zaid@stackroute.in>
2024-07-24 20:24:25 +0530

    first push

---
+++ src/test/java/com/itc/jenkins/JenkinsDeleteMeApplicationTests.java
@@ -0,0 +1,20 @@
+package com.itc.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDeleteMeApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDeleteMeApplicationTests.class);
+
+	@Test
+	public void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true, true);
+	}
+}
